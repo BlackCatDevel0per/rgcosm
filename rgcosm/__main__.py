66bb12f79684ff8e86f3bb66500d8bfcde9eea2e
Asad Sayfiyev <bcdev@mail.ru>
2023-04-14 03:59:50 +0500

    Fix cli import

--- rgcosm/__main__.py
+++ rgcosm/__main__.py
@@ -6,8 +6,8 @@ if len(sys.argv) == 1:
 
 import argparse
 
-from convert import parser as conv_parser
-from geocoder import parser as ltln_parser
+from .convert import parser as conv_parser
+from .geocoder import parser as ltln_parser
 
 parser = argparse.ArgumentParser(description='rgcosm cli', parents=[conv_parser, ltln_parser])
 
@@ -16,16 +16,16 @@ args = parser.parse_args()
 
 # Converter
 if args.cinput:
-	from convert import osm2sqlite3
-	from convert import init_args as converter_init_args
+	from .convert import osm2sqlite3
+	from .convert import init_args as converter_init_args
 	converter_init_args(args)
 	osm2sqlite3(args.cinput, args.coutput, args.add_indexes)
 
 
 # Geocoder
 elif args.database:
-	from geocoder import get_address
-	from geocoder import init_args as geocoder_init_args
+	from .geocoder import get_address
+	from .geocoder import init_args as geocoder_init_args
 	geocoder_init_args(args)
 	addr = get_address(args.database, (args.latitude, args.longitude), args.search_tags, args.min_tags_count)
 	print(addr)
